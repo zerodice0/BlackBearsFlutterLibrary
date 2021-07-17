6f747f59bae8dc668c48641bde82634ea0644879
zerodice0 <zerodice0@gmail.com>
2021-07-17 19:52:52 +0900

    [add] - add _buildTableRow for building TableRow template. - add state _daumPostcodeSearchDataModel. - add Card widget for displaying result.
    [fix]
    - remove async keyword from main.
    - call DaumPostcodeSearch.initialize function from initState for initializing InAppWebView.

--- lib/library/daum_postcode_search/example/lib/main.dart
+++ lib/library/daum_postcode_search/example/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:daum_postcode_search/daum_postcode_search.dart';
 import 'package:example/searching_page.dart';
 import 'package:flutter/material.dart';
 
-void main() async {
+void main() {
   runApp(MyApp());
 }
 
@@ -30,8 +30,31 @@ class DaumPostcodeSearchExample extends StatefulWidget {
 }
 
 class _DaumPostcodeSearchExampleState extends State<DaumPostcodeSearchExample> {
+  DataModel? _daumPostcodeSearchDataModel;
+
+  @override
+  void initState() {
+    super.initState();
+
+    DaumPostcodeSearch.initialize();
+  }
+
   @override
   Widget build(BuildContext context) {
+    TableRow _buildTableRow(String label, String value) {
+      return TableRow(
+        children: [
+          TableCell(
+            verticalAlignment: TableCellVerticalAlignment.middle,
+            child: Text(label, textAlign: TextAlign.center),
+          ),
+          TableCell(
+            child: Text(value, textAlign: TextAlign.center),
+          ),
+        ],
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -45,16 +68,89 @@ class _DaumPostcodeSearchExampleState extends State<DaumPostcodeSearchExample> {
             children: <Widget>[
               ElevatedButton.icon(
                 onPressed: () async {
-                  DataModel result = await Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => SearchingPage(),
-                    ),
-                  );
+                  try {
+                    DataModel model = await Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => SearchingPage(),
+                      ),
+                    );
+                    print(_daumPostcodeSearchDataModel);
 
-                  print(result);
+                    setState(
+                      () {
+                        _daumPostcodeSearchDataModel = model;
+                      },
+                    );
+                  } catch (error) {
+                    print(error);
+                  }
                 },
                 icon: Icon(Icons.search),
                 label: Text("주소 검색"),
+              ),
+              Visibility(
+                visible: _daumPostcodeSearchDataModel != null,
+                child: Card(
+                  child: Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.all(10),
+                          child: RichText(
+                            text: TextSpan(
+                              style:
+                                  TextStyle(color: Colors.black, fontSize: 20),
+                              children: [
+                                WidgetSpan(
+                                  child: Icon(
+                                    Icons.check_circle,
+                                    color: Theme.of(context).accentColor,
+                                  ),
+                                ),
+                                TextSpan(text: "주소 검색 결과"),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Table(
+                          border: TableBorder.symmetric(
+                              inside: BorderSide(color: Colors.grey)),
+                          columnWidths: {
+                            0: FlexColumnWidth(1),
+                            1: FlexColumnWidth(2),
+                          },
+                          children: [
+                            _buildTableRow(
+                              "한글주소",
+                              _daumPostcodeSearchDataModel?.address ?? "",
+                            ),
+                            _buildTableRow(
+                              "영문주소",
+                              _daumPostcodeSearchDataModel?.addressEnglish ??
+                                  "",
+                            ),
+                            _buildTableRow(
+                              "우편번호",
+                              _daumPostcodeSearchDataModel?.zonecode ?? "",
+                            ),
+                            _buildTableRow(
+                              "지번주소",
+                              _daumPostcodeSearchDataModel?.autoJibunAddress ??
+                                  "",
+                            ),
+                            _buildTableRow(
+                              "지번주소(영문)",
+                              _daumPostcodeSearchDataModel
+                                      ?.autoJibunAddressEnglish ??
+                                  "",
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
               ),
             ],
           ),

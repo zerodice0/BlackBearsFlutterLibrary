2866d7d7b7d9645b6631af5c27840f0ace2833d9
zerodice0 <zerodice0@gmail.com>
2021-07-18 16:13:10 +0900

    [add] - change SearchingPage widget to StatefulWidget for handling error.
    [fix]
    - add onLoadError, onLoadHttpError callback to example main.dart.
    - displaying refresh button if _isError state is true.

--- lib/library/daum_postcode_search/example/lib/main.dart
+++ lib/library/daum_postcode_search/example/lib/main.dart
@@ -158,16 +158,62 @@ class _DaumPostcodeSearchExampleState extends State<DaumPostcodeSearchExample> {
   }
 }
 
-class SearchingPage extends StatelessWidget {
+class SearchingPage extends StatefulWidget {
+  @override
+  _SearchingPageState createState() => _SearchingPageState();
+}
+
+class _SearchingPageState extends State<SearchingPage> {
+  bool _isError = false;
+  String? errorMessage;
+
   @override
   Widget build(BuildContext context) {
-    DaumPostcodeSearch daumPostcodeSearch = DaumPostcodeSearch();
+    DaumPostcodeSearch daumPostcodeSearch = DaumPostcodeSearch(
+      onConsoleMessage: (_, message) => print(message),
+      onLoadError: (controller, uri, errorCode, message) => setState(
+        () {
+          _isError = true;
+          errorMessage = message;
+        },
+      ),
+      onLoadHttpError: (controller, uri, errorCode, message) => setState(
+        () {
+          _isError = true;
+          errorMessage = message;
+        },
+      ),
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: Text("주소 검색 페이지입니다."),
         centerTitle: true,
       ),
-      body: daumPostcodeSearch,
+      body: Container(
+        child: Column(
+          children: [
+            Expanded(
+              child: daumPostcodeSearch,
+            ),
+            Visibility(
+              visible: _isError,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(errorMessage ?? ""),
+                  ElevatedButton(
+                    child: Text("Refresh"),
+                    onPressed: () {
+                      daumPostcodeSearch.controller?.reload();
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
